ddceefa4296ea2fac79e8e06acd77ef147066484
Rinat G <107737@gmail.com>
2024-08-23 17:36:00 +0700

    update Dockerfile

--- middleware/pingprophet/database.py
+++ middleware/pingprophet/database.py
@@ -6,8 +6,8 @@ import os
 class DatabaseWrapper:
     def __init__(self):
         db_host = os.getenv('DB_HOST', 'localhost')
-        db_name = os.getenv('DB_NAME', 'middleware')
-        db_user = os.getenv('DB_USER', 'middleware')
+        db_name = os.getenv('DB_DATABASE', 'middleware')
+        db_user = os.getenv('DB_USERNAME', 'middleware')
         db_password = os.getenv('DB_PASSWORD', 'middleware')
         db_port = os.getenv('DB_PORT', '5432')
         self.database = Database(f"postgresql+asyncpg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
